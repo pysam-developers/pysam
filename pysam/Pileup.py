01497979bfdc84a5cf8effb9b17e7994e7752cd5
andreas@fgu210.anat.ox.ac.uk <andreas@fgu210.anat.ox.ac.uk>
2009-09-10 13:27:12 +0100

    added PileupIndex

--- pysam/Pileup.py
+++ pysam/Pileup.py
@@ -2,8 +2,8 @@
 import collections
 import pysam
 
-PileupEntry = collections.namedtuple( "PileupEntry",
-                                      " ".join( (\
+PileupSubstitution = collections.namedtuple( "PileupSubstitution",
+                                    " ".join( (\
             "chromosome", 
             "position", 
             "reference_base", 
@@ -15,21 +15,46 @@ PileupEntry = collections.namedtuple( "PileupEntry",
             "read_bases",
             "base_qualities" ) ) )
 
+PileupIndel = collections.namedtuple( "PileupIndel",
+                                      " ".join( (\
+            "chromosome", 
+            "position", 
+            "reference_base", 
+            "genotype",
+            "consensus_quality",
+            "snp_quality",
+            "rms_mapping_quality",
+            "coverage",
+            "first_allelle",
+            "second_allele",
+            "reads_first",
+            "reads_second",
+            "reads_diff" ) ) )
+
 def iterate( infile ):
     '''iterate over ``samtools pileup -c`` formatted file.
 
     *infile* can be any iterator over a lines.
 
-    The function yields objects of the type :class:`pysam.Pileup.PileupEntry`.
+    The function yields named tuples of the type :class:`pysam.Pileup.PileupSubstitution`
+    or :class:`pysam.Pileup.PileupIndel`.
 
     .. note:: 
        The parser converts to 0-based coordinates
     '''
     
-    conv = (str,lambda x: int(x)-1,str,str,int,int,int,int,str,str)
+    conv_subst = (str,lambda x: int(x)-1,str,str,int,int,int,int,str,str)
+    conv_indel = (str,lambda x: int(x)-1,str,str,int,int,int,int,str,str,int,int,int)
 
     for line in infile:
-        try:
-            yield PileupEntry( *[x(y) for x,y in zip(conv,line[:-1].split()) ] )
-        except TypeError:
-            raise pysam.SamtoolsError( "parsing error in line: `%s`" % line)
+        d = line[:-1].split()
+        if d[2] == "*":
+            try:
+                yield PileupIndel( *[x(y) for x,y in zip(conv_indel,d) ] )
+            except TypeError:
+                raise pysam.SamtoolsError( "parsing error in line: `%s`" % line)
+        else:
+            try:
+                yield PileupSubstitution( *[x(y) for x,y in zip(conv_subst,d) ] )
+            except TypeError:
+                raise pysam.SamtoolsError( "parsing error in line: `%s`" % line)
