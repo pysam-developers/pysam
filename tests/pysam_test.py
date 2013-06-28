290d014e860ad13da025c30d96671a79788d9af4
andreas.heger@gmail.com <andreas.heger@gmail.com>
2013-06-28 06:37:27 +0100

    fixed tests

--- tests/pysam_test.py
+++ tests/pysam_test.py
@@ -975,7 +975,8 @@ class TestHeaderBam(TestHeaderSam):
 
 class TestHeader1000Genomes( unittest.TestCase ):
 
-    bamfile = "http://ftp.1000genomes.ebi.ac.uk/vol1/ftp/technical/phase2b_alignment/data/NA07048/exome_alignment/NA07048.unmapped.ILLUMINA.bwa.CEU.exome.20120522_p2b.bam"
+    # bamfile = "http://ftp.1000genomes.ebi.ac.uk/vol1/ftp/technical/phase2b_alignment/data/NA07048/exome_alignment/NA07048.unmapped.ILLUMINA.bwa.CEU.exome.20120522_p2b.bam"
+    bamfile = "http://ftp.1000genomes.ebi.ac.uk/vol1/ftp/technical/phase3_EX_or_LC_only_alignment/data/HG00104/alignment/HG00104.chrom11.ILLUMINA.bwa.GBR.low_coverage.20130415.bam"
         
     def testRead( self ):
 
