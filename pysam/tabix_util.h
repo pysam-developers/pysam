6eb48cacfe280169b10d2b48141974c896acea56
andreas.heger@gmail.com <andreas.heger@gmail.com>
2013-10-05 22:49:54 +0100

    tabix_util.h - added unistd include

--- pysam/tabix_util.h
+++ pysam/tabix_util.h
@@ -2,5 +2,6 @@
    On some MACOSX systems getline is not defined.
  */
 #if !(_POSIX_C_SOURCE >= 200809L || _XOPEN_SOURCE >= 700)
+#include "unistd.h"
 ssize_t getline(char **line, size_t *linelen, FILE *fp);
 #endif
