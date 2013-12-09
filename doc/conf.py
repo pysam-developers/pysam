3f90c12c49e97e34c8292a0401f320101a9f1e34
andreas.heger@gmail.com <andreas.heger@gmail.com>
2013-12-09 14:53:37 +0000

    renamed contents.rst to index.rst
    --HG--
    rename : doc/contents.rst => doc/index.rst

--- doc/conf.py
+++ doc/conf.py
@@ -41,7 +41,7 @@ source_suffix = '.rst'
 #source_encoding = 'utf-8'
 
 # The master toctree document.
-master_doc = 'contents'
+master_doc = 'index'
 
 # General information about the project.
 project = u'pysam'
@@ -130,7 +130,7 @@ html_theme = 'default'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = []
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
@@ -182,7 +182,7 @@ htmlhelp_basename = 'samtoolsdoc'
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
-  ('contents', 'samtools.tex', ur'samtools Documentation',
+  ('index', 'pysam.tex', ur'pysam documentation',
    ur'Andreas Heger, Tildon Grant Belgrad, Martin Goodson', 'manual'),
 ]
 
