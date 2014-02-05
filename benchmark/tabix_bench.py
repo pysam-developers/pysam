dbd629557b62078218244bb52a1004c6e141266a
andreas.heger@gmail.com <andreas.heger@gmail.com>
2014-02-05 20:39:01 +0000

    tabix_bench.py - py3 compatibility

--- benchmark/tabix_bench.py
+++ benchmark/tabix_bench.py
@@ -4,7 +4,7 @@ import timeit
 
 iterations = 5
 repeats = 100
-print "repeats=", repeats, "iterations=", iterations
+print ("repeats=", repeats, "iterations=", iterations)
 
 fn_compressed = '/tmp/windows_small.bed.gz'
 fn_uncompressed = '/tmp/windows_small.bed'
@@ -12,7 +12,7 @@ fn_uncompressed = '/tmp/windows_small.bed'
 def test_python_compressed():
     '''iterate through with python.'''
     f = gzip.open( fn_compressed)
-    l = len( [x.split("\t") for x in f])
+    l = len( [x.encode().split("\t") for x in f])
  
 def test_python_uncompressed():
     '''iterate through with python.'''
@@ -65,12 +65,12 @@ tests = ( test_python_compressed,
           test_iterator_file_uncompressed )
 
 for repeat in range( repeats ):
-    print "# repeat=", repeat
+    print ("# repeat=", repeat)
     for test in tests:
         try:
             t = timeit.timeit( test, number = iterations )
         except AttributeError:
             continue
-        print "%5.2f\t%s" % (t,str(test))
+        print ("%5.2f\t%s" % (t,str(test)))
 
 
