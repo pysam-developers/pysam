7a41a5fdadc93e9942607936fec0c1b9a0c8a629
Andreas Heger <andreas.heger@gmail.com>
2016-01-15 21:17:12 +0000

    add bcftools to travis setup

--- install-CGAT-tools.sh
+++ install-CGAT-tools.sh
@@ -161,6 +161,13 @@ cd samtools-1.3
 make
 PATH=$PATH:$HOME/CGAT/external-tools/samtools-1.3
 
+# install bcftools
+curl -L http://downloads.sourceforge.net/project/bcftools/bcftools/1.3/bcftools-1.3.tar.bz2 > bcftools-1.3.tar.bz2
+tar xjvf bcftools-1.3.tar.bz2 
+cd bcftools-1.3
+make
+PATH=$PATH:$HOME/CGAT/external-tools/bcftools-1.3
+
 popd
 
 } # nosetests_external_deps
