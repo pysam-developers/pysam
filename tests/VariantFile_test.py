568ecb368c869a30b883e205c6300b72ac48de1c
Andreas Heger <andreas.heger@gmail.com>
2016-01-18 16:53:32 +0000

    comment out incomplete test

--- tests/VariantFile_test.py
+++ tests/VariantFile_test.py
@@ -6,37 +6,38 @@ import pysam
 DATADIR="cbcf_data"
 from tabix_test import loadAndConvert
 
-class TestMissingGenotypes(unittest.TestCase):
 
-    filename = "missing_genotypes.vcf"
+# class TestMissingGenotypes(unittest.TestCase):
 
-    def setUp(self):
-        self.compare = loadAndConvert(os.path.join(DATADIR, self.filename),
-                                      encode=False)
+#     filename = "missing_genotypes.vcf"
 
-    def check(self, filename):
-        return 
-        fn = os.path.join(DATADIR, filename)
-        self.assertEqual(True, os.path.exists(fn))
-        # v = pysam.VariantFile(fn)
-        # for site in v:
-        #     for ss,rec in site.samples.items():
-        #         print ss, rec
+#     def setUp(self):
+#         self.compare = loadAndConvert(os.path.join(DATADIR, self.filename),
+#                                       encode=False)
 
-        v = pysam.VariantFile(fn)
-        for x, site in enumerate(v):
-            import pdb; pdb.set_trace()
-            print "======"
-            print site
-            for ss,rec in site.samples.items():
-                print ss
-                print rec.alleles
+#     def check(self, filename):
+#         return 
+#         fn = os.path.join(DATADIR, filename)
+#         self.assertEqual(True, os.path.exists(fn))
+#         # v = pysam.VariantFile(fn)
+#         # for site in v:
+#         #     for ss,rec in site.samples.items():
+#         #         print ss, rec
 
-    def testVCF(self):
-        self.check(self.filename)
+#         v = pysam.VariantFile(fn)
+#         for x, site in enumerate(v):
+#             import pdb; pdb.set_trace()
+#             print "======"
+#             print site
+#             for ss,rec in site.samples.items():
+#                 print ss
+#                 print rec.alleles
 
-    def testVCFGZ(self):
-        self.check(self.filename + ".gz")
+#     def testVCF(self):
+#         self.check(self.filename)
+
+#     def testVCFGZ(self):
+#         self.check(self.filename + ".gz")
 
 
 if __name__ == "__main__":
