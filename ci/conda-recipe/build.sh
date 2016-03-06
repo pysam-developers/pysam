acb42bdefa45dc79e7b1dec5dac797f131146a83
Kyle Beauchamp <kyleabeauchamp@gmail.com>
2016-03-06 12:33:21 -0800

    Cleanup

--- ci/conda-recipe/build.sh
+++ ci/conda-recipe/build.sh
@@ -8,16 +8,28 @@ export HTSLIB_CONFIGURE_OPTIONS="--disable-libcurl"
 # Use --old-and-unmanageable to disable egg-style folder naming, which is a minor hassle for RPATH stuff.
 $PYTHON setup.py install --old-and-unmanageable
 
-otool -L ${SP_DIR}/pysam/*.so
-
-install_name_tool -change @rpath/pysam/libchtslib.so @rpath/python2.7/site-packages/pysam/libchtslib.so ${SP_DIR}/pysam/calignedsegment.so
-install_name_tool -change @rpath/pysam/libchtslib.so @rpath/python2.7/site-packages/pysam/libchtslib.so ${SP_DIR}/pysam/calignmentfile.so
-install_name_tool -change @rpath/pysam/libchtslib.so @rpath/python2.7/site-packages/pysam/libchtslib.so ${SP_DIR}/pysam/cbcf.so
-install_name_tool -change @rpath/pysam/libchtslib.so @rpath/python2.7/site-packages/pysam/libchtslib.so ${SP_DIR}/pysam/cfaidx.so
-install_name_tool -change @rpath/pysam/libchtslib.so @rpath/python2.7/site-packages/pysam/libchtslib.so ${SP_DIR}/pysam/csamfile.so
-install_name_tool -change @rpath/pysam/libchtslib.so @rpath/python2.7/site-packages/pysam/libchtslib.so ${SP_DIR}/pysam/ctabix.so
-install_name_tool -change @rpath/pysam/libchtslib.so @rpath/python2.7/site-packages/pysam/libchtslib.so ${SP_DIR}/pysam/ctabixproxies.so
-install_name_tool -change @rpath/pysam/libchtslib.so @rpath/python2.7/site-packages/pysam/libchtslib.so ${SP_DIR}/pysam/cutils.so
-install_name_tool -change @rpath/pysam/libchtslib.so @rpath/python2.7/site-packages/pysam/libchtslib.so ${SP_DIR}/pysam/cvcf.so
-
-otool -L ${SP_DIR}/pysam/*.so
+# Hacky workaround to fix rpath pathing issues
+if [ "$(uname)" == "Darwin" ]; then
+  otool -L ${SP_DIR}/pysam/*.so
+  install_name_tool -change @rpath/pysam/libchtslib.so @rpath/python${PY_VER}/site-packages/pysam/libchtslib.so ${SP_DIR}/pysam/calignedsegment.so
+  install_name_tool -change @rpath/pysam/libchtslib.so @rpath/python${PY_VER}/site-packages/pysam/libchtslib.so ${SP_DIR}/pysam/calignmentfile.so
+  install_name_tool -change @rpath/pysam/libchtslib.so @rpath/python${PY_VER}/site-packages/pysam/libchtslib.so ${SP_DIR}/pysam/cbcf.so
+  install_name_tool -change @rpath/pysam/libchtslib.so @rpath/python${PY_VER}/site-packages/pysam/libchtslib.so ${SP_DIR}/pysam/cfaidx.so
+  install_name_tool -change @rpath/pysam/libchtslib.so @rpath/python${PY_VER}/site-packages/pysam/libchtslib.so ${SP_DIR}/pysam/csamfile.so
+  install_name_tool -change @rpath/pysam/libchtslib.so @rpath/python${PY_VER}/site-packages/pysam/libchtslib.so ${SP_DIR}/pysam/ctabix.so
+  install_name_tool -change @rpath/pysam/libchtslib.so @rpath/python${PY_VER}/site-packages/pysam/libchtslib.so ${SP_DIR}/pysam/ctabixproxies.so
+  install_name_tool -change @rpath/pysam/libchtslib.so @rpath/python${PY_VER}/site-packages/pysam/libchtslib.so ${SP_DIR}/pysam/cutils.so
+  install_name_tool -change @rpath/pysam/libchtslib.so @rpath/python${PY_VER}/site-packages/pysam/libchtslib.so ${SP_DIR}/pysam/cvcf.so
+  otool -L ${SP_DIR}/pysam/*.so
+else
+	CURRENT_RPATH=`patchelf --print-rpath ${SP_DIR}/pysam/cvcf.so`
+  patchelf --set-rpath ./:$CURRENT_RPATH ${SP_DIR}/pysam/calignedsegment.so
+  patchelf --set-rpath ./:$CURRENT_RPATH ${SP_DIR}/pysam/calignmentfile.so
+  patchelf --set-rpath ./:$CURRENT_RPATH ${SP_DIR}/pysam/cbcf.so
+  patchelf --set-rpath ./:$CURRENT_RPATH ${SP_DIR}/pysam/cfaidx.so
+  patchelf --set-rpath ./:$CURRENT_RPATH ${SP_DIR}/pysam/csamfile.so
+  patchelf --set-rpath ./:$CURRENT_RPATH ${SP_DIR}/pysam/ctabix.so
+  patchelf --set-rpath ./:$CURRENT_RPATH ${SP_DIR}/pysam/ctabixproxies.so
+  patchelf --set-rpath ./:$CURRENT_RPATH ${SP_DIR}/pysam/cutils.so
+  patchelf --set-rpath ./:$CURRENT_RPATH ${SP_DIR}/pysam/cvcf.so
+fi
