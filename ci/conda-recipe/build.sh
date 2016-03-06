5d0df743d892e2b9160ff6d0ff64804adb5014c1
Kyle Beauchamp <kyleabeauchamp@gmail.com>
2016-03-06 11:25:35 -0800

    Fixes

--- ci/conda-recipe/build.sh
+++ ci/conda-recipe/build.sh
@@ -2,7 +2,22 @@
 
 # Use internal htslib
 chmod a+x ./htslib/configure
-export CFLAGS="-I${PREFIX}/include/curl/ -I${PREFIX}/include -L${PREFIX}/lib"
+export CFLAGS="-I${PREFIX}/include -L${PREFIX}/lib"
 export HTSLIB_CONFIGURE_OPTIONS="--disable-libcurl"
 
-$PYTHON setup.py install
+# Use --old-and-unmanageable to disable egg-style folder naming, which is a minor hassle for RPATH stuff.
+$PYTHON setup.py install --old-and-unmanageable
+
+otool -L ${SP_DIR}/pysam/*.so
+
+install_name_tool -change @rpath/pysam/libchtslib.so @rpath/python2.7/site-packages/pysam/libchtslib.so ${SP_DIR}/pysam/calignedsegment.so
+install_name_tool -change @rpath/pysam/libchtslib.so @rpath/python2.7/site-packages/pysam/libchtslib.so ${SP_DIR}/pysam/calignmentfile.so
+install_name_tool -change @rpath/pysam/libchtslib.so @rpath/python2.7/site-packages/pysam/libchtslib.so ${SP_DIR}/pysam/cbcf.so
+install_name_tool -change @rpath/pysam/libchtslib.so @rpath/python2.7/site-packages/pysam/libchtslib.so ${SP_DIR}/pysam/cfaidx.so
+install_name_tool -change @rpath/pysam/libchtslib.so @rpath/python2.7/site-packages/pysam/libchtslib.so ${SP_DIR}/pysam/csamfile.so
+install_name_tool -change @rpath/pysam/libchtslib.so @rpath/python2.7/site-packages/pysam/libchtslib.so ${SP_DIR}/pysam/ctabix.so
+install_name_tool -change @rpath/pysam/libchtslib.so @rpath/python2.7/site-packages/pysam/libchtslib.so ${SP_DIR}/pysam/ctabixproxies.so
+install_name_tool -change @rpath/pysam/libchtslib.so @rpath/python2.7/site-packages/pysam/libchtslib.so ${SP_DIR}/pysam/cutils.so
+install_name_tool -change @rpath/pysam/libchtslib.so @rpath/python2.7/site-packages/pysam/libchtslib.so ${SP_DIR}/pysam/cvcf.so
+
+otool -L ${SP_DIR}/pysam/*.so
