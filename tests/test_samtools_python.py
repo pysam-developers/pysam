d0a2f598a9b1ae16a36e9824e9167bdc6d221e2a
Kyle Beauchamp <kyleabeauchamp@gmail.com>
2016-03-22 21:09:48 -0700

    Fix test on py3k

--- tests/test_samtools_python.py
+++ tests/test_samtools_python.py
@@ -12,7 +12,7 @@ def test_bedcov_split_lines():
     bed_filename = "./pysam_data/ex1.bed"
     lines = pysam.bedcov(bed_filename, bam_filename, split_lines=True)  # Test pysam 0.8.X style output, which returns a list of lines
     for line in lines:
-        fields = line.split('\t')
+        fields = str(line).split('\t')  # Need to cast byte to string for py3k
         assert len(fields) in [4, 5], "bedcov should give tab delimited output with 4 or 5 fields.  Split line (%s) gives %d fields." % (fields, len(fields))
 
 def test_idxstats_parse():
@@ -20,7 +20,7 @@ def test_idxstats_parse():
     idxstats_string = pysam.idxstats(bam_filename, split_lines=False)  # Test pysam 0.9.X style output, which returns a string that needs to be split by \n
     lines = idxstats_string.splitlines()
     for line in lines:
-        splt = line.split("\t")
+        splt = str(line).split("\t")  # Need to cast byte to string for py3k
         _seqname, _seqlen, nmapped, _nunmapped = splt
 
 
@@ -30,5 +30,5 @@ def test_bedcov():
     bedcov_string = pysam.bedcov(bed_filename, bam_filename, split_lines=False)  # Test pysam 0.9.X style output, which returns a string that needs to be split by \n
     lines = bedcov_string.splitlines()
     for line in lines:
-        fields = line.split('\t')
+        fields = str(line).split('\t')  # Need to cast byte to string for py3k
         assert len(fields) in [4, 5], "bedcov should give tab delimited output with 4 or 5 fields.  Split line (%s) gives %d fields." % (fields, len(fields))
