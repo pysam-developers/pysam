0a679223fdf9a87c532d8a0f7b6b78328728e93b
Andreas Heger <andreas.heger@gmail.com>
2016-06-14 15:07:24 +0100

    {AH} fix typo

--- tests/samtools_test.py
+++ tests/samtools_test.py
@@ -222,7 +222,7 @@ class SamtoolsTest(unittest.TestCase):
     def testStatements(self):
         for statement in self.statements:
             if (statement.startswith("calmd") and 
-                list(sys.version_info[:2]) = [3,3]):
+                list(sys.version_info[:2]) == [3, 3]):
                 # skip calmd test, fails only on python 3.3.5
                 # in linux (empty output). Works in OsX and passes
                 # for 3.4 and 3.5
