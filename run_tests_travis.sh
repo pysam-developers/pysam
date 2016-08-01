80ccda9d4dd2764afd25c9776d6a736819e70bf6
Andreas Heger <andreas.heger@gmail.com>
2016-08-01 17:21:31 +0100

    {AH} use curl retry option

--- run_tests_travis.sh
+++ run_tests_travis.sh
@@ -34,9 +34,7 @@ mkdir -p $WORKDIR/external-tools
 
 # install htslib
 cd $WORKDIR/external-tools
-for i in 1 2 3 4 5; do
-    curl -L https://github.com/samtools/htslib/releases/download/1.3.1/htslib-1.3.1.tar.bz2 > htslib-1.3.1.tar.bz2 && break || sleep 15
-done
+curl --retry-delay 60 --retry 5 -L https://github.com/samtools/htslib/releases/download/1.3.1/htslib-1.3.1.tar.bz2 > htslib-1.3.1.tar.bz2
 tar xjvf htslib-1.3.1.tar.bz2
 cd htslib-1.3.1
 make
@@ -45,9 +43,7 @@ LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$WORKDIR/external-tools/htslib-1.3.1
 
 # install samtools, compile against htslib
 cd $WORKDIR/external-tools
-for i in 1 2 3 4 5; do
-    curl -L http://downloads.sourceforge.net/project/samtools/samtools/1.3.1/samtools-1.3.1.tar.bz2 > samtools-1.3.1.tar.bz2 && break || sleep 15
-done
+curl --retry-delay 60 --retry 5 -L http://downloads.sourceforge.net/project/samtools/samtools/1.3.1/samtools-1.3.1.tar.bz2 > samtools-1.3.1.tar.bz2
 
 tar xjvf samtools-1.3.1.tar.bz2
 cd samtools-1.3.1
@@ -64,9 +60,7 @@ fi
 
 # install bcftools
 cd $WORKDIR/external-tools
-for i in 1 2 3 4 5; do
-   curl -L https://github.com/samtools/bcftools/releases/download/1.3.1/bcftools-1.3.1.tar.bz2 > bcftools-1.3.1.tar.bz2 && break || sleep 15
-done
+curl --retry-delay 60 --retry 5 -L https://github.com/samtools/bcftools/releases/download/1.3.1/bcftools-1.3.1.tar.bz2 > bcftools-1.3.1.tar.bz2
 
 tar xjf bcftools-1.3.1.tar.bz2
 cd bcftools-1.3.1
