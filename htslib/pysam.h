429ba3947870a6a97897b118f9034dee9766ee08
Andreas Heger <andreas.heger@gmail.com>
2016-11-05 22:43:12 +0000

    {AH} upgrade to htslib 1.3.2

--- htslib/pysam.h
+++
@@ -1,7 +0,0 @@
-#ifndef PYSAM_H
-#define PYSAM_H
-#include "stdio.h"
-extern FILE * pysam_stderr;
-extern FILE * pysam_stdout;
-extern int PYSAM_STDOUT_FILENO;
-#endif
--- htslib/version.h
+++ htslib/version.h
@@ -1,1 +1,1 @@
-#define HTS_VERSION "1.3.1"
+#define HTS_VERSION "1.3.2"
