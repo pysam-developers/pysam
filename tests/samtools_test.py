9d7c10ec29466ffb6b37af6fe56d0f371d50be3c
Andreas Heger <andreas.heger@gmail.com>
2017-04-10 09:42:43 +0100

    {AH} fix typo

--- tests/samtools_test.py
+++ tests/samtools_test.py
@@ -237,7 +237,7 @@ class SamtoolsTest(unittest.TestCase):
                 continue
             self.check_statement(statement)
 
-    @unitest.skipIf(sys.platform == "darwin", "not supported, pattern does not match")
+    @unittest.skipIf(sys.platform == "darwin", "not supported, pattern does not match")
     def testUsage(self):
         if self.executable == "bcftools":
             # bcftools usage messages end with exit(1)
