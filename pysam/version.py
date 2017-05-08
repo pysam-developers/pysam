9b266f495eabb5cf62414f8a7303c1e308c9b70d
Andreas Heger <andreas.heger@gmail.com>
2017-05-08 13:25:34 +0100

    {AH} upgrade to htslib 1.4.1

--- pysam/version.py
+++ pysam/version.py
@@ -1,9 +1,9 @@
 # pysam versioning information
 
-__version__ = "0.11"
+__version__ = "0.11.2"
 
 __samtools_version__ = "1.4"
 
 __bcftools_version__ = "1.4"
 
-__htslib_version__ = "1.4"
+__htslib_version__ = "1.4.1"
