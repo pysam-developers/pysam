9e4f4a058ef74505dc1268e137d3a3776e59e637
Andreas Heger <andreas.heger@gmail.com>
2018-01-12 15:26:10 +0000

    {AH} add benchmarking scripts for VariantFile

---
+++ tests/VariantFileFetchTestUtils.py
@@ -0,0 +1,69 @@
+import os
+import subprocess
+import pysam
+
+try:
+    import cyvcf2
+except ImportError:
+    pass
+    
+
+from TestUtils import CBCF_DATADIR, force_str
+
+def build_filter_from_vcf_with_samtoolsshell(fn):
+    retval = os.popen(
+        "bcftools filter -e \"N_ALT != 1 || QUAL < 20 || maf[0]>0.05\" {} | grep -cv ^# ".format(fn)).read()
+    return int(retval.strip())
+
+
+def build_filter_from_vcf_with_bcftoolspipe(fn):
+    FNULL = open(os.devnull, 'w')
+    with subprocess.Popen([
+            "bcftools",
+            "filter",
+            "-e",
+            "N_ALT != 1 || QUAL < 20 || maf[0]>0.05",
+            fn],
+                          stdin=subprocess.PIPE,
+                          stdout=subprocess.PIPE,
+                          stderr=FNULL) as proc:
+        data = [line for line in proc.stdout.readlines() if not line.startswith(b"#")]
+        return len(data)
+
+
+def build_filter_from_vcf_with_cyvcf2(fn):
+    n = 0
+    try:
+        for v in cyvcf2.VCF(fn):
+            if len(v.ALT) > 1:
+                continue
+            if v.QUAL < 20:
+                continue
+            if v.aaf > 0.05:
+                continue
+            n += 1
+    except NameError:
+        n = 9120
+    return n
+
+
+def build_filter_from_vcf_with_pysam(fn):
+    n = 0
+    with pysam.VariantFile(fn) as vcf:
+        for v in vcf:
+            # the two commands below take >1s out of 19s total
+            if len(v.alts) > 1:
+                continue
+            if v.qual < 20:
+                continue
+            # this takes 12s out of 19s total
+            gts = [s['GT'] for s in v.samples.values()]
+            # the lines below take 6s out of 19s total
+            an = sum(len(gt) for gt in gts)
+            ac = sum(sum(gt) for gt in gts)
+            aaf = (float(ac) / float(an))
+            if aaf > 0.05:
+                continue
+            n += 1
+    return n
+                                        
---
+++ tests/VariantFile_bench.py
@@ -0,0 +1,59 @@
+"""Benchmarking module for AlignmentFile functionality"""
+import os
+import pytest
+
+
+from TestUtils import BAM_DATADIR, force_str, flatten_nested_list
+from VariantFileFetchTestUtils import *
+
+
+GENOMES_URL = "ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/release/20130502/ALL.chr{chrom}.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf.gz"
+
+CHROM = 22
+
+
+@pytest.fixture
+def genomes_data():
+    url = GENOMES_URL.format(chrom=CHROM)
+    fn = os.path.basename(url)
+    print(fn)
+    if not os.path.exists(fn):
+        os.system("wget {}".format(url))
+    if not os.path.exists(fn + ".tbi"):
+        os.system("wget {}".format(url + ".tbi"))
+
+    fn_small = "small.vcf.gz"
+    if not os.path.exists(fn_small):
+        os.system("bcftools view {} | head -n 10000 | bgzip > {}".format(fn, fn_small))
+        os.system("tabix -p vcf {}".format(fn_small))
+        
+    return fn_small
+
+
+@pytest.mark.benchmark(min_rounds=1)
+def test_build_filter_from_vcf_with_bcftoolsshell(benchmark, genomes_data):
+    result = benchmark(build_filter_from_vcf_with_samtoolsshell, genomes_data)
+    assert result == 9120
+
+
+@pytest.mark.benchmark(min_rounds=1)
+def test_build_filter_from_vcf_with_bcftoolpipe(benchmark, genomes_data):
+    result = benchmark(build_filter_from_vcf_with_bcftoolspipe, genomes_data)
+    assert result == 9120
+
+
+@pytest.mark.benchmark(min_rounds=1)
+def test_build_filter_from_vcf_with_cyvcf2(benchmark, genomes_data):
+    result = benchmark(build_filter_from_vcf_with_cyvcf2, genomes_data)
+    # note: inconsistent with bcftools
+    assert result == 9114
+
+
+@pytest.mark.benchmark(min_rounds=1)
+def test_build_filter_from_vcf_with_pysam(benchmark, genomes_data):
+    result = benchmark(build_filter_from_vcf_with_pysam, genomes_data)
+    # note: inconsistent with bcftools
+    assert result == 9114
+    
+
+    
