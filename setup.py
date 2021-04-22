245c6fd386daa94b651da9fe9740ff61c24b204b
Marcel Martin <marcel.martin@scilifelab.se>
2021-04-22 18:32:02 +0200

    Start to add static typing

--- setup.py
+++ setup.py
@@ -452,7 +452,7 @@ metadata = {
     'ext_modules': [Extension(**opts) for opts in modules],
     'cmdclass': cmdclass,
     'package_dir': package_dirs,
-    'package_data': {'': ['*.pxd', '*.h'], },
+    'package_data': {'': ['*.pxd', '*.h', 'py.typed', '*.pyi'], },
     # do not pack in order to permit linking to csamtools.so
     'zip_safe': False,
     'use_2to3': True,
