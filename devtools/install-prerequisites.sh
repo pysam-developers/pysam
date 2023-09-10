861072b72c8abd3471316ff10a7a5422011220de
John Marshall <jmarshall@hey.com>
2023-09-10 12:27:33 +1200

    Refactor pysam wheel building
    Redesign job matrix to build x86+ARM macOS wheels in one job, and
    i686+x86_64+ARM Linux wheels in principle in one job -- but subdivide
    by Python version because each emulated ARM build takes 1.5 hours.
    (Whereas ARM macOS builds are fast because they are cross-compiled.)
    
    No need to set up Python or prerequisites outside cibuildwheel.
    Use a separate script to install prerequisites regardless of the
    package manager in use. Use CIBW_* variables carefully so that the
    same job step handles both Linux and macOS.
    
    Update cibuildwheel, and ensure we build a complete set of wheels
    from cp36 to the upcoming cp312.

---
+++ devtools/install-prerequisites.sh
@@ -0,0 +1,34 @@
+#!/bin/sh -e
+
+if test -x /usr/bin/dnf; then
+    echo Installing prerequisites via dnf...
+    dnf -y install epel-release
+    dnf -y install zlib-devel bzip2-devel xz-devel curl-devel samtools bcftools htslib-tools
+
+elif test -x /usr/bin/yum; then
+    if yum -y install epel-release; then
+        echo Installing prerequisites via yum...
+        yum -y install zlib-devel bzip2-devel xz-devel curl-devel samtools bcftools htslib-tools
+    else
+        echo Installing non-test prerequisites via yum...
+        yum -y install zlib-devel bzip2-devel xz-devel curl-devel
+    fi
+
+elif test -d /etc/dpkg; then
+    echo Installing prerequisites via apt-get...
+    apt-get update
+    apt-get install -y --no-install-recommends --no-install-suggests libcurl4-openssl-dev zlib1g-dev libbz2-dev liblzma-dev samtools bcftools tabix
+
+elif test -x /sbin/apk; then
+    echo Installing non-test prerequisites via apk...
+    apk update
+    apk add zlib-dev bzip2-dev xz-dev curl-dev
+
+elif test -x ${HOMEBREW_PREFIX-/usr/local}/bin/brew; then
+    echo Installing prerequisites via brew...
+    HOMEBREW_NO_AUTO_UPDATE=1 brew install -q samtools bcftools
+    brew unlink xz || true
+
+else
+    echo No package manager detected
+fi
