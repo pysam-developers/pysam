bf5b85e0e172ed7669a85bb31f9e6cecef3e5e90
John Marshall <jmarshall@hey.com>
2024-10-20 19:47:03 +1300

    Add tests that check type hints by invoking mypy via its API
    As each test is executed (thus verifying the actual code too), it invokes
    typecheck(). This runs mypy on the function that invoked it, parses the
    reveal_locals() output, and optionally checks that each local's hinted
    type is the same as its actual runtime time. This is optional because
    some tests require more nuanced checks; cf test_pileup_iterator_column()
    in which pileup()'s runtime type is a subclass of the hinted type.
    
    Add test cases for previously merged PRs #1273, #1296, and #1313.
    
    Also fix some pysam/libcbcf.pyi type hints that caused these tests
    to fail for unrelated reasons.

---
+++ tests/typechecking_test.py
@@ -0,0 +1,123 @@
+import inspect
+import os
+import pytest
+import re
+from typing import TYPE_CHECKING
+
+import pysam
+import pysam.samtools
+import pysam.bcftools
+
+from TestUtils import BAM_DATADIR, make_data_files
+
+try:
+    import mypy.api
+except ImportError:
+    pytest.skip('mypy API not available', allow_module_level=True)
+
+PREAMBLE = """
+import re
+from typing import TYPE_CHECKING
+
+import pysam
+import pysam.samtools
+import pysam.bcftools
+
+def typecheck(check_locals = True): pass
+"""
+
+MYPY_OPTIONS = ['--no-incremental', '--no-error-summary', '--follow-imports', 'silent']
+
+
+def typecheck(check_locals: bool = True):
+    myframe = inspect.currentframe()
+    if not myframe: pytest.skip('current stack frame not available')
+    caller = myframe.f_back
+    if not caller: pytest.skip('caller stack frame not available')
+
+    code = inspect.getsource(caller)
+    stdout, stderr, status = mypy.api.run(MYPY_OPTIONS + ['--command', PREAMBLE + code])
+    assert status == 0, f'mypy failed:\n{stdout}{stderr}'
+
+    types = {}
+    for line in stdout.splitlines():
+        m = re.search(r'note:   *(\w+): ([\w.]*)', line)
+        if m: types[m.group(1)] = m.group(2)
+
+    def _plain(s):
+        s = re.sub(r"<class '([^']*)'>", r'\1', s)
+        s = re.sub(r'builtins\.', '', s)
+        return s
+
+    if check_locals:
+        for var, vartype in types.items():
+            assert _plain(vartype) == _plain(repr(type(caller.f_locals[var]))), f'Incorrect type for {var!r}'
+
+    return types
+
+
+def setUpModule():
+    make_data_files(BAM_DATADIR)
+
+
+@pytest.fixture
+def aln():
+    header = pysam.AlignmentHeader.from_references(['chr1', 'chr2'], [50000, 20000])
+    a = pysam.AlignedSegment(header)
+    a.query_name = 'read_one'
+    a.flag = pysam.FPAIRED | pysam.FREAD1
+    a.reference_id = 0
+    a.reference_start = 1000
+    a.mapping_quality = 20
+    a.next_reference_id = 1
+    a.next_reference_start = 5000
+    a.template_length = 0
+    a.cigartuples = ((pysam.CMATCH, 6), (pysam.CINS, 4), (pysam.CMATCH, 2))
+    a.query_sequence = 'ATGCATGCATGC'
+    a.query_qualities = pysam.qualitystring_to_array('abcdefghijkl')
+    return a
+
+
+@pytest.fixture
+def sam_fname():
+    return os.path.join(BAM_DATADIR, 'ex3.sam')
+
+
+@pytest.fixture
+def bam_fname():
+    return os.path.join(BAM_DATADIR, 'ex1.bam')
+
+
+def test_AlignedSegment_is_mapped(aln: pysam.AlignedSegment) -> None:
+    rmap = aln.is_mapped
+    mmap = aln.mate_is_mapped
+    rfwd = aln.is_forward
+    mfwd = aln.mate_is_forward
+
+    if TYPE_CHECKING: reveal_locals()
+    types = typecheck()
+
+
+def test_AlignmentHeader_get_reference_length(sam_fname: str) -> None:
+    inf = pysam.AlignmentFile(sam_fname)
+    n1 = inf.get_reference_length('chr1')
+    hdr = inf.header
+    n2 = hdr.get_reference_length('chr1')
+
+    if TYPE_CHECKING: reveal_locals()
+    types = typecheck()
+
+
+def test_pileup_iterator_column(bam_fname: str) -> None:
+    inf = pysam.AlignmentFile(bam_fname)
+    pu = inf.pileup('chr1')
+    for p in pu:
+        pid = p.reference_id
+        ppos = p.reference_pos
+
+    if TYPE_CHECKING: reveal_locals()
+    types = typecheck(check_locals=False)
+    assert re.search(r'\.IteratorColumn(|All|AllRefs|Region)$', types['pu'])
+    assert types['p'].endswith('PileupColumn')
+    assert types['pid'] == 'builtins.int'
+    assert types['ppos'] == 'builtins.int'
