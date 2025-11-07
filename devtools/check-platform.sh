3db05f9e7e8be9718cd8008af012010fba65f4c8
John Marshall <jmarshall@hey.com>
2025-11-07 22:35:52 +1300

    Update release workflows for Python 3.14, Intel macOS runner, etc
    Add Python 3.14 to release workflows and update cibuildwheel to a release
    that uses 3.14-final. Use the new macos-15-intel image instead of macos-13,
    which is obsolete.
    
    Reorganise the build matrix to separate the manylinux and musllinux jobs
    so they run in parallel.
    
    Move all wheel building to GitHub Actions: Cirrus CI's macos-sonoma-base's
    default Python can no longer run current cibuildwheel (which requires 3.11),
    so it is time to move the ARM wheels to be built on GitHub Actions, now that
    that supports both Linux and macOS on ARM.

--- devtools/check-platform.sh
+++ devtools/check-platform.sh
@@ -1,21 +1,11 @@
 #!/bin/sh -e
 
-case $1 in
-    ubuntu-*-arm)  expected=arm ;;
-    macos-13)      expected=x86_64 ;;
-    ubuntu-*)      expected=x86_64 ;;
-    macos-*)       expected=arm ;;
-    windows-*)     expected=x86_64 ;;
-    *)
-	echo Unknown platform $1 >&2
-	exit 2
-	;;
-esac
+expected=$1
 
 arch=$(uname -m)
 case $arch in
     arm*|aarch*)  actual=arm ;;
-    x86*)         actual=x86_64 ;;
+    x86*)         actual=intel ;;
     *)
 	echo Unrecognized uname result $arch >&2
 	exit 2
