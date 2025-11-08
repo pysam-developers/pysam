184b61d2381fab61c42670a06af841ea811c4178
Benjamin Yeh <bentyeh@users.noreply.github.com>
2025-11-08 01:44:02 -0800

    bugfix for previous commit: missing trailing doublequote

--- pysam/libcalignedsegment.pyx
+++ pysam/libcalignedsegment.pyx
@@ -2524,7 +2524,7 @@ cdef class AlignedSegment:
             # validate Hex values
             if not HEX_VALUE_REGEX.fullmatch(value):
                 raise ValueError(
-                    f"Invalid value {value} for tag {tag.decode()} with value_type 'H':
+                    f"Invalid value {value} for tag {tag.decode()} with value_type 'H': "
                     f"must match the regular expression {HEX_VALUE_REGEX.pattern}"
                 )
             # Note that hex tags are stored the very same
